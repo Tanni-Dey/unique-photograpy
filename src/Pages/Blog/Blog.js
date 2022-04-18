d09d6ff095f3a6ae7303dd6bddd6d5cb6ef530f6
Tanni-Dey <tonnidey277615@gmail.com>
2022-04-18 20:44:58 +0600

    blogs

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -2,12 +2,43 @@ import React from 'react';
 
 const Blog = () => {
     return (
-        <div>
-            <h1>Blog</h1>
-            <div className='row'>
+        <div className='container'>
+            <h1>Blogs</h1>
+            <div style={{ textAlign: 'left' }} className='row gx-5 py-5'>
                 <div className='col-md-4'>
                     <div>
-
+                        <h5>Q1: Difference between `authorization` and `authentication`</h5>
+                        <p>Authentication is a procces that verify who is someone with valid email and password or any social media login. On the other hand, Authorization id a process of verifying that user have access to something. Authentication done before authorization. Authentication check user valid email an password, Authorization check user security system.</p>
+                    </div>
+                </div>
+                <div className='col-md-4'>
+                    <div>
+                        <h5>Q2:  Why are you using `firebase`? What other options do you have to implement authentication?</h5>
+                        <p>Firebase is a backend system where stored data for web or mobile application, ios ,mobile game. Its using for employment authorization system of app. It's a remote database where keep stored user data, message etc  with secure. No need extra money spent for using firebase. It's show website crashing report for any error. Firebase is easier, faster than other authentication system thats why using firebase in any application.
+                            <h5>Other option for authentication:
+                            </h5>
+                            <li>Back4App</li>
+                            <li>AWS Amplify</li>
+                            <li>Kuzzle</li>
+                            <li>Couchbase</li>
+                            <li>Flutter</li>
+                            <li>RxDB</li>
+                        </p>
+                    </div>
+                </div>
+                <div className='col-md-4'>
+                    <div>
+                        <h5>Q3: What other services does `firebase` provide other than authentication?</h5>
+                        <p>
+                            <h5>Other option of Firebase:
+                            </h5>
+                            <li>Cloud Firestore</li>
+                            <li>Cloud Functions</li>
+                            <li>Hosting</li>
+                            <li>Cloud Storage</li>
+                            <li>Predictions</li>
+                            <li>Cloud Messaging</li>
+                        </p>
                     </div>
                 </div>
             </div>
